378ebd6dcaf45b5885b43ee0729abb6ce2b10e2a
Rushi Gawali <rushi_g@tekditechnologies.com>
2023-06-10 09:45:50 +0530

    refactor of code and added new credentials independent api in ulp-reference

--- src/portal/portal.controller.ts
+++ src/portal/portal.controller.ts
@@ -58,4 +58,83 @@ export class PortalController {
       response,
     );
   }
+  @Post('/credentials')
+  async credentials(
+    @Headers('Authorization') auth: string,
+    @Body('did') did: string,
+    @Res() response: Response,
+  ) {
+    const jwt = auth.replace('Bearer ', '');
+    return this.portalService.credentials(jwt, did, response);
+  }
+  @Post('/credentials/render')
+  async renderCredentials(
+    @Headers('Authorization') auth: string,
+    @Body() requestbody: any,
+    @Res({ passthrough: true }) response,
+  ): Promise<string | StreamableFile> {
+    const jwt = auth.replace('Bearer ', '');
+    response.header('Content-Type', 'application/pdf');
+    return this.portalService.renderCredentials(jwt, requestbody);
+  }
+  @Post('/credentials/renderhtml')
+  async renderCredentialsHTML(
+    @Headers('Authorization') auth: string,
+    @Body() requestbody: any,
+    @Res() response: Response,
+  ) {
+    const jwt = auth.replace('Bearer ', '');
+    return this.portalService.renderCredentialsHTML(jwt, requestbody, response);
+  }
+  @Get('/credentials/rendertemplate/:id')
+  async renderTemplate(@Param('id') id: string, @Res() response: Response) {
+    return this.portalService.renderTemplate(id, response);
+  }
+  @Get('/credentials/rendertemplateschema/:id')
+  async renderTemplateSchema(
+    @Param('id') id: string,
+    @Res() response: Response,
+  ) {
+    return this.portalService.renderTemplateSchema(id, response);
+  }
+  //new credentials list schema id schema template
+  //credentialsSearch
+  @Post('/credentials/search/:type')
+  async credentialsSearch(
+    @Headers('Authorization') auth: string,
+    @Param('type') type: string,
+    @Body() requestbody: any,
+    @Res() response: Response,
+  ) {
+    const jwt = auth.replace('Bearer ', '');
+    return this.portalService.credentialsSearch(
+      jwt,
+      type,
+      requestbody,
+      response,
+    );
+  }
+  //credentialsIssue
+  @Post('/credentials/issue')
+  async credentialsIssue(
+    @Headers('Authorization') auth: string,
+    @Body() requestbody: any,
+    @Res() response: Response,
+  ) {
+    const jwt = auth.replace('Bearer ', '');
+    return this.portalService.credentialsIssue(jwt, requestbody, response);
+  }
+  //credentialsSchema
+  @Get('/credentials/schema/:id')
+  async credentialsSchema(@Param('id') id: string, @Res() response: Response) {
+    return this.portalService.credentialsSchema(id, response);
+  }
+  //credentialsSchemaJSON
+  @Get('/credentials/schema/json/:id')
+  async credentialsSchemaJSON(
+    @Param('id') id: string,
+    @Res() response: Response,
+  ) {
+    return this.portalService.credentialsSchemaJSON(id, response);
+  }
 }
